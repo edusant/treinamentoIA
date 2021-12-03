2459364961e12662bf01f7dc7f72b85ac9464f6c
Eduardo Santos Silva <eduardos_silva@hotmail.com>
2021-12-03 16:50:19 -0300

    analise de dados

---
+++ analisededados/preprocessamento/index.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+
+dadoscredit = pd.read_csv('credit_data.csv')
+print(dadoscredit)
